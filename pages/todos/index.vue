27481db48ad37429c88fd0932be1dd473c77b4c9
takehiko ogasawara <>
2019-06-24 23:17:14 +0900

    todoアプリ製作中

---
+++ pages/todos/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <ul>
+        <li v-for="todo in todos" :key="todo.id">
+            <input type="checkbox" :checked="todo.done"
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
